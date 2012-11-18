a57d6dd3428dfc9a8d813fa1773043dca9989240
Botffy <scipiades.armin@gmail.com>
2012-11-18 14:50:01 +0100

    Basic Terminal structure.
    Also removes the binary, sorry about that.

--- src/terminal.cpp
+++ src/terminal.cpp
@@ -1,8 +1,26 @@
 #include "terminal.hpp"
 
 namespace sum {
-	std::string Terminal::command(const std::string& input) {
-		return input+"\n";
+	namespace {
+		std::string whitespace = " \t\n\r";	
+	}
+	
+	std::string Terminal::command(std::string input) {
+		// trim left
+		size_t s = input.find_first_not_of(whitespace);
+		if(std::string::npos != s) input.substr(s).swap(input);
+
+		if(input.empty()) return "";
+
+		// get command -- everything before the first space.
+		s = input.find_first_of(whitespace);
+		std::string command = input.substr( 0, s );
+
+		if("pwd" == command) {
+			return get_working_directory()+"\n";
+		}
+
+		return command+": command not found\n";
 	}	
 	
 	std::string Terminal::get_working_directory() {
--- src/terminal.hpp
+++ src/terminal.hpp
@@ -6,11 +6,11 @@ namespace sum {
 
 class Terminal {
 	public:
-		// Bemenetként kap egy stringet, ez a sor, amit a user beírt. Feltesszük, hogy nem üres.
+		// Bemenetként kap egy stringet, ez a sor, amit a user beírt.
 		// ezt a stringet feldolgozzuk, végrehajtjuk a parancsot,
 		// és visszaadjuk a választ, amit kiírunk a képernyőre.
 		// A válasz sikeres végrehajtás esetén üres string.
-		std::string command(const std::string& input);
+		std::string command(std::string input);
 
 		// Visszaadja az aktuális patht, pl "/bin" vagy "/spells/buffs" vagy "/"
 		std::string get_working_directory();

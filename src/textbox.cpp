441364d547407849f5f11c7e2b56ff9ccc01e648
Badics Áron Gergely <badar@badar-ThinkPad-R61.(none)>
2012-11-25 20:00:16 +0100

    starting textbox. fixes some stuff

---
+++ src/textbox.cpp
@@ -0,0 +1,5 @@
+#include "textbox.hpp"
+
+namespace sum
+{
+}
---
+++ src/textbox.hpp
@@ -0,0 +1,18 @@
+#ifndef TEXTBOX_HPP
+#define TEXTBOX_HPP
+
+#include "widget.hpp"
+
+namespace sum
+{
+
+class TextBox : public Widget{
+
+public:
+
+private:
+
+};
+
+}
+#endif

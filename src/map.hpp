b003704580c67259aa6168db80efecd74f393f50
Badics Áron <badics.aron@gmail.com>
2013-01-17 19:34:25 +0100

    added robots sprites stuff.

--- src/map.hpp
+++ src/map.hpp
@@ -19,6 +19,30 @@ public:
 
 private:
 
+	sf::Image robot_image;
+	sf::Sprite robot_sprite;
+	static const int SPRITE_SIZE=32;
+
+	enum Facing {down=0,left,right,up};
+
+	struct Robot
+	{
+		Robot(int _ID,int _team, int _x, int _y):
+			ID(_ID),
+			team(_team),
+			x(_x),
+			y(_y),
+			facing(down){}
+
+		int ID;
+		int x,y;
+		int team;
+		Facing facing;
+	};
+
+	std::vector<Robot> robots;
+
+	void draw_robots();
 
 };
 

bc3c0fc23c28cbc1ce39525a8ea41e1f2073d523
Badics Áron <badics.aron@gmail.com>
2013-02-01 11:38:18 +0100

    combatlog displays HP losses

--- src/combatlog.cpp
+++ src/combatlog.cpp
@@ -5,6 +5,18 @@ namespace sum
 
 void CombatLog::update(const ServerMessage &message)
 {
+	using namespace stringutils;
+
+	std::vector<std::string> res = message.get_parsed_msg();
+
+	switch(message.type)
+	{
+		case ServerMessage::hp_loss:
+			add("Puppet with ID:" + res[0] + " lost " + res[1] + " hp.");
+		break;
+		default:
+		break;
+	}
 
 }
 
